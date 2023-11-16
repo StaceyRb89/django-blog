d73cbaa5c7285b2cd32da16e3cfb3882b8664fce
Stacey robson <stacey.robson.sr@gmail.com>
2023-11-16 11:45:58 +0000

    secret key added

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,10 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(zt1n_cd!z#rang97ynm+klmfi$tm-@*urye4vqahk3w$c+s0-'
+#SECRET_KEY = 'django-insecure-(zt1n_cd!z#rang97ynm+klmfi$tm-@*urye4vqahk3w$c+s0-'
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-staceyrb89-djangoblog-s7m88g4ak8r.ws-eu106.gitpod.io', '.herokuapp.com']
 
--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgres://ywcsatqg:xrkWV7KT4ycGF1W0oNPp3l2_uchtQPBj@flora.db.elephantsql.com/ywcsatqg")
+
+os.environ.setdefault("SECRET_KEY", "zt1n_cd!z#rang97ynm+klmfi$tm-@*urye4vqahk3w$c+s0-")

c4310ea5ad00efedd08ed1240fbdc07a41a7512f
Stacey robson <stacey.robson.sr@gmail.com>
2023-11-17 22:59:47 +0000

    recovered files

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path("<int:event_id>/", views.post_detail,
-         name="event_detail") 
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
 ]

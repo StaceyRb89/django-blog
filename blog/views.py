540d39aa8512a3a81148666a39e24e6da5023988
Stacey robson <stacey.robson.sr@gmail.com>
2023-11-20 12:02:07 +0000

    everything working i think?

--- blog/views.py
+++ blog/views.py
@@ -6,10 +6,27 @@ from .models import Post, Comment
 from .forms import CommentForm
 
 # Create your views here.
+
+
 class PostList(generic.ListView):
+    """
+    Returns all published posts in :model:`blog.Post`
+    and displays them in a page of six posts. 
+    **Context**
+
+    ``queryset``
+        All published instances of :model:`blog.Post`
+    ``paginate_by``
+        Number of posts per page.
+        
+    **Template:**
+
+    :template:`blog/index.html`
+    """
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
+
 
 def post_detail(request, slug):
     """
@@ -19,20 +36,23 @@ def post_detail(request, slug):
 
     ``post``
         An instance of :model:`blog.Post`.
+    ``comments``
+        All approved comments related to the post.
+    ``comment_count``
+        A count of approved comments related to the post.
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`
 
     **Template:**
 
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
     if request.method == "POST":
-        print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
-        print("About to render template")
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
             comment.author = request.user
@@ -41,22 +61,34 @@ def post_detail(request, slug):
             messages.add_message(
                 request, messages.SUCCESS,
                 'Comment submitted and awaiting approval'
-    )
+            )
+    
     comment_form = CommentForm()
 
     return render(
         request,
         "blog/post_detail.html",
-        {   "post": post,
+        {
+            "post": post,
             "comments": comments,
             "comment_count": comment_count,
-            "comment_form": comment_form,
-    },
-)
+            "comment_form": comment_form
+        },
+    )
+
 
 def comment_edit(request, slug, comment_id):
     """
-    view to edit comments
+    Display an individual comment for edit.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post.
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`
     """
     if request.method == "POST":
 
@@ -72,27 +104,32 @@ def comment_edit(request, slug, comment_id):
             comment.save()
             messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
         else:
-            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+            messages.add_message(request, messages.ERROR,
+                                 'Error updating comment!')
 
     return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
 
-    def review_edit(request, event_id, review_id):
+def comment_delete(request, slug, comment_id):
+    """
+    Delete an individual comment.
 
-        if request.method == "POST":
+    **Context**
 
-            queryset = Event.objects.all()
-            event = get_object_or_404(queryset, pk=event_id)
-            review = get_object_or_404(Review, pk=review_id)
-            review_form = ReviewForm(data=request.POST, instance=review)  
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post.
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
 
-        if review_form.is_valid() and review.reviewer == request.user:
-            review = review_form.save(commit=False)
-            review.reviewer = request.user
-            review.event = event
-            review.save()
-            messages.add_message(request, messages.SUCCESS, 'Review Updated!')
-        else:
-            messages.add_message(request, messages.ERROR, 'Error updating Review!')
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR,
+                             'You can only delete your own comments!')
 
-    return HttpResponseRedirect(reverse('event_detail', args=[event_id]))
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
--- codestar/settings.py
+++ codestar/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -46,14 +47,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary_storage',
     'django.contrib.sites',
-    'django_summernote',
-    'cloudinary',
-    'blog',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'crispy_forms',
     'crispy_bootstrap5',
+    'django_summernote',
+    'cloudinary',
+    'blog',
     'about',
 ]
 
